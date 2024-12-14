9ba16e0e8019751a3df5d1bf5fbdbff28fb10d96
Edson1337 <ed1337pi@hotmail.com>
2024-12-13 23:42:06 -0300

    BugFix: Adjustment to the Brand model and creation of the migration to the database with the updates. Also, correctly adding Brand to the Admin route.

--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
 
 class BrandAdmin(admin.ModelAdmin):
@@ -10,4 +10,5 @@ class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model', 'brand')
 
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin)
